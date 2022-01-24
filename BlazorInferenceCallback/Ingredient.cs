6f06c4481512b42e76714aea3e7accb8aadde3a3
Mario van Zeist <m.vanzeist@home.nl>
2022-01-24 19:57:33 +0100

    Add project files.

---
+++ BlazorInferenceCallback/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorInferenceCallback
+{
+    public class Ingredient : IIngredient
+    {
+        public static IEnumerable<Ingredient> Ingredients { get; } = new Ingredient[] { "Mango", "Garlic", "Sugar" , "Honey" };
+        public string Name { get; init; } = default!;
+
+        public static implicit operator Ingredient(string name)
+            => new() { Name = name };
+        public override string ToString()
+            => Name;
+    }
+
+    public interface IIngredient
+    {
+        public string Name { get; }
+    }
+}
